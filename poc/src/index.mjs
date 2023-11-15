f643bebae8643f16d307302eb96f38a90c8f856e
Angelina Maurer <angelina.maurer@smail.th-koeln.de>
2023-11-15 15:41:06 +0100

    CorrectionPoCFirebase

--- poc/src/index.mjs
+++ poc/src/index.mjs
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set } from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAFjj-U3Ylj5daf__Zzq3wllb4GiRF3kio",
   authDomain: "ep-poc-f1041.firebaseapp.com",
